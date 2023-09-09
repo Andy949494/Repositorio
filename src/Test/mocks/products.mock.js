b8d2c3024c94bf474bc1f94037100360da0810b2
Andrés Orlando <andresorlando3@gmail.com>
2023-09-08 21:01:34 -0300

    Add files via upload

---
+++ src/Test/mocks/products.mock.js
@@ -0,0 +1,53 @@
+const prodcut1 = {
+    title: 'a',
+    description: 'a',
+    code: 'a1',
+    price: 1,
+    status: 'true',
+    stock: '1',
+    category: 'a',
+    owner: 'admin',
+    thumbnails: ["http://localhost:8080/images/1685902988367-1.jpg"]
+}
+
+const product2 = {
+    title: 'b',
+    description: 'b',
+    code: 'b2',
+    price: '2',
+    status: 'true',
+    stock: '2',
+    category: 'b',
+    owner: 'admin',
+    thumbnails: ["http://localhost:8080/images/1685902988367-2.jpg"]
+}
+
+const product3 = {
+    title: 'c',
+    description: 'c',
+    code: 'c3',
+    price: '3',
+    status: 'true',
+    stock: '3',
+    category: 'c',
+    owner: 'admin',
+    thumbnails: ["http://localhost:8080/images/1685902988367-3.jpg"]
+}
+
+const productWithoutTitle = {
+    description: 'd',
+    code: 'd4',
+    price: '4',
+    status: 'true',
+    stock: '4',
+    category: 'd',
+    owner: 'admin',
+    thumbnails: ["http://localhost:8080/images/1685902988367-4.jpg"]
+}
+
+export {
+    prodcut1,
+    product2,
+    product3,
+    productWithoutTitle
+}
